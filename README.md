c83d3e66cf17d33ea4c724eaa98f751e68691067
Cyril <github@ituilerie.com>
2014-05-25 17:22:53 +0200

    1st Commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Change_Planning_NetAtmo_Thermostat
+Switch_Planning_NetAtmo_Thermostat
 ==================================
 
 Changement de planning hebdomadaire de chauffage sur le thermostat NetAtmo
+
+L'appel de la fonction ce fait par URL
+
+http://xxxxxxxxx/thermostat_switch_planning.php?plan=1    remplacer 1 par votre ID de planning
+
+
+Pour l'instant il vous faudrat aller chercher vous meme l'ID de votre planning 
---
+++ thermostat_switch_planning.php
@@ -0,0 +1,79 @@
+<?php
+
+// Cyril E      http://www.ituilerie.com
+// Ecriture de données au thermostat NetAtmo
+
+// Appel par l'url
+// http://xxxxxxxxx/thermostat_switch_planning.php?plan=1    remplacer 1 par votre ID de planning
+
+
+$planning=$_GET['plan'];   // Non du planning
+
+
+$password='xxxxxxxx';
+$username='xxxxxxxxx';
+
+
+$app_id = 'xxxxxxxxx';
+$app_secret = 'xxxxxxxxxxx';
+
+
+$token_url = "https://api.netatmo.net/oauth2/token";
+$postdata = http_build_query(
+        array(
+            'grant_type' => "password",
+            'client_id' => $app_id,
+            'client_secret' => $app_secret,
+            'username' => $username,
+            'password' => $password,
+            'scope' => 'read_station read_thermostat write_thermostat'
+    )
+);
+
+$opts = array('http' =>
+	array(
+		'method'  => 'POST',
+		'header'  => 'Content-type: application/x-www-form-urlencoded',
+		'content' => $postdata
+	)
+);
+
+$context  = stream_context_create($opts);
+$response = file_get_contents($token_url, false, $context);
+
+$params = null;
+$params = json_decode($response, true);
+$api_url = "https://api.netatmo.net/api/getuser?access_token=" . $params['access_token']."&app_type=app_thermostat";
+$requete = @file_get_contents($api_url);
+
+$url_devices = "https://api.netatmo.net/api/devicelist?access_token=" .  $params['access_token']."&app_type=app_thermostat";
+$resulat_device = @file_get_contents($url_devices);	
+
+$json_devices = json_decode($resulat_device,true);
+
+$device1 = $json_devices["body"]["devices"][0]["_id"];
+$module1 = $json_devices["body"]["modules"][0]["_id"];
+$device2 = $json_devices["body"]["devices"][1]["_id"];
+$module2 = $json_devices["body"]["modules"][1]["_id"];
+$device3 = $json_devices["body"]["devices"][2]["_id"];
+$module3 = $json_devices["body"]["modules"][2]["_id"];
+
+
+// Non utilisé pour l'instant
+//$url_thermostat1="http://api.netatmo.net/api/getthermstate?access_token=" .  $params['access_token']."&device_id=".$device1."&module_id=".$module1;
+//$value_thermostat1= file_get_contents($url_thermostat1);
+//$json_mesures_thermostat1 = json_decode($value_thermostat1, true);
+//$progid = $json_mesures_thermostat1["body"]["therm_program_backup"]["timetable"]["program_id"];
+
+	
+$url="/api/switchschedule?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&schedule_id=".$planning;
+    
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://api.netatmo.net".$url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+curl_close($ch);
+
+
+?>
